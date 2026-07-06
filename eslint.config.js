84128361718f0ed785aa97db340fac18260d9c95
Osiris-of-IRIS <jmQeP+git@posteo.eu>
2026-07-06 15:07:32 +0200

    Add files via upload

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+// Flat ESLint config. Decision IDs: ADR-0001, ADR-0002.
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+
+export default tseslint.config(
+  { ignores: ['dist', 'coverage', 'test-results'] },
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    },
+  },
+);
